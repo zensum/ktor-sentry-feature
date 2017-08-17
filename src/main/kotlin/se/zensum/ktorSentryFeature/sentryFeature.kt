9a6e150e396cb53272f25479f5e75dca40fcbb19
William Rudenmalm <me@whn.se>
2017-08-18 01:15:50 +0200

    Sentry: Use static Sentry interface & add customization
    The library now attempts to retain user preferences when Sentry.init
    is called outside our code. In fact we now Sentry.init IFF the user
    specified a custom DSN.

--- src/main/kotlin/se/zensum/ktorSentryFeature/sentryFeature.kt
+++ src/main/kotlin/se/zensum/ktorSentryFeature/sentryFeature.kt
@@ -1,26 +1,39 @@
 package se.zensum.ktorSentry
 
 import io.sentry.Sentry
-import org.jetbrains.ktor.application.ApplicationCall
+import io.sentry.SentryClient
 import org.jetbrains.ktor.application.ApplicationCallPipeline
 import org.jetbrains.ktor.application.ApplicationFeature
 import org.jetbrains.ktor.pipeline.PipelineContext
 import org.jetbrains.ktor.util.AttributeKey
 
-class SentryFeature(configuration: Configuration) {
+private typealias CustomizeFn = SentryClient.() -> Unit
 
-    val sentryClient = Sentry.init(configuration.dsn).apply {environment = configuration.appEnv }
+class SentryFeature(configuration: Configuration) {
 
     class Configuration {
         var dsn: String? = null
-        var appEnv: String = "n/a"
+        var appEnv: String? = null
+        var customizeFn: CustomizeFn? = null
+        fun customize(fn: CustomizeFn) { customizeFn = fn }
+
+        internal fun initClient() {
+            // If the user has specified a
+            if (dsn != null) {
+                Sentry.init(dsn)
+            }
+            if (appEnv != null) {
+                Sentry.getStoredClient().environment = appEnv
+            }
+            customizeFn?.invoke(Sentry.getStoredClient())
+        }
     }
 
     suspend fun intercept(context: PipelineContext<Unit>) {
-        try{
+        try {
             context.proceed()
-        }catch (e: Exception){
-            sentryClient.sendException(e)
+        } catch (e: Exception) {
+            Sentry.capture(e)
             throw e
         }
     }
@@ -28,7 +41,10 @@ class SentryFeature(configuration: Configuration) {
     companion object Feature : ApplicationFeature<ApplicationCallPipeline, Configuration, SentryFeature> {
         override val key = AttributeKey<SentryFeature>("SentryFeature")
         override fun install(pipeline: ApplicationCallPipeline, configure: Configuration.() -> Unit): SentryFeature {
-            val result = SentryFeature(Configuration().apply(configure))
+            val cfg = Configuration().apply(configure)
+            cfg.initClient()
+            val result = SentryFeature(cfg)
+
             pipeline.intercept(ApplicationCallPipeline.Call) {
                 result.intercept(this)
             }
